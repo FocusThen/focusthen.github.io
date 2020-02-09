fbcf185f9545d496da46f807884bb3b85bf2aa0c
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2020-02-09 21:41:56 +0300

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const finalPath = path.resolve(__dirname, 'dist')
+const finalPath = path.resolve(__dirname, "dist");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/js/index.js',
+    mode: "development",
+    entry: "./src/js/index.js",
     output: {
         path: finalPath,
-        filename: 'app.js',
+        filename: "app.js",
+        publicPath: "/"
     },
     resolve: {
         alias: {
-            img: path.resolve(__dirname, 'src/img'),
-            js: path.resolve(__dirname, 'src/js'),
-            font: path.resolve(__dirname, 'src/fonts'),
-        },
+            img: path.resolve(__dirname, "src/img"),
+            js: path.resolve(__dirname, "src/js"),
+            font: path.resolve(__dirname, "src/fonts")
+        }
+    },
+    devServer: {
+        contentBase: "./dist"
     },
     module: {
         rules: [
@@ -25,66 +29,63 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
+                        presets: ["@babel/preset-env"]
+                    }
+                }
             },
             {
                 test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/,
                 use: [
                     {
-                        loader: 'file-loader',
+                        loader: "file-loader",
                         options: {
-                            name: '[name].[ext]',
-                            outputPath: 'img',
-                        },
-                    },
-                ],
+                            name: "[name].[ext]",
+                            outputPath: "img"
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(woff|woff2|ttf|otf|eot)$/,
                 use: [
                     {
-                        loader: 'file-loader',
+                        loader: "file-loader",
                         options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts',
-                        },
-                    },
-                ],
+                            name: "[name].[ext]",
+                            outputPath: "fonts"
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(glsl|vs|fs|vert|frag)$/,
                 exclude: /node_modules/,
-                use: [
-                    'raw-loader',
-                    'glslify-loader',
-                ],
-            },
-        ],
+                use: ["raw-loader", "glslify-loader"]
+            }
+        ]
     },
     plugins: [
         new CopyPlugin([
-            { from: './src/fonts', to: path.join(finalPath, '/fonts'), force: true },
-            { from: './src/img', to: path.join(finalPath, '/img'), force: true },
+            {
+                from: "./src/fonts",
+                to: path.join(finalPath, "/fonts"),
+                force: true
+            },
+            { from: "./src/img", to: path.join(finalPath, "/img"), force: true }
         ]),
         new MiniCssExtractPlugin({
-            filename: 'app.css',
+            filename: "app.css"
         }),
         new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            server: { baseDir: '.' },
-        }),
-    ],
-}
+            host: "http://focusthen.github.io",
+            port: process.env.PORT || 3000,
+            server: { baseDir: "." }
+        })
+    ]
+};
