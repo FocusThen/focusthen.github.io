b975e03ab5319005fead205f57c01331b1e09844
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2020-02-13 00:24:59 +0300

    testing0

---
+++ precache-manifest.7a4a7de29cadd34327a3d76f43616622.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e30416f66c9be62ef4cb58af534ad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec541f328713211232c",
+    "url": "/static/css/main.22ef3ece.chunk.css"
+  },
+  {
+    "revision": "90d81048d29e3d4f5255",
+    "url": "/static/js/2.6ef899a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6ef899a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ec541f328713211232c",
+    "url": "/static/js/main.df33b545.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "09110907da064ab3af260a324ddf2b5b",
+    "url": "/static/media/dev-jane-01.09110907.jpg"
+  },
+  {
+    "revision": "a9dcff5e78678cbdef12278e19786262",
+    "url": "/static/media/dev-jane-02.a9dcff5e.jpg"
+  },
+  {
+    "revision": "6f8d345507e607bc80d92919bac09487",
+    "url": "/static/media/portfolio-01.6f8d3455.jpg"
+  },
+  {
+    "revision": "238af781fa615dcb77e87d8044ce9716",
+    "url": "/static/media/portfolio-02.238af781.jpg"
+  },
+  {
+    "revision": "216d2ddeee981170884b6950685444fa",
+    "url": "/static/media/portfolio-03.216d2dde.jpg"
+  },
+  {
+    "revision": "a58fb775c252e4253d32aab13337be33",
+    "url": "/static/media/portfolio-04.a58fb775.jpg"
+  },
+  {
+    "revision": "84961aff027ddb358e721a472ae4e197",
+    "url": "/static/media/portfolio-05.84961aff.jpg"
+  },
+  {
+    "revision": "4eadc66ee1128c8644dbfc9e1a00d224",
+    "url": "/static/media/portfolio-06.4eadc66e.jpg"
+  },
+  {
+    "revision": "7909856bf323a3d7215763bed46d6083",
+    "url": "/static/media/portfolio-07.7909856b.jpg"
+  },
+  {
+    "revision": "c49509933c876210ee46b9a42fc266e4",
+    "url": "/static/media/portfolio-08.c4950993.jpg"
+  },
+  {
+    "revision": "b04311b24758eaccc317015265e3e71d",
+    "url": "/static/media/portfolio-09.b04311b2.jpg"
+  },
+  {
+    "revision": "1c291f59bb1965138c57e9c3f4a48128",
+    "url": "/static/media/portfolio-10.1c291f59.jpg"
+  },
+  {
+    "revision": "97891b5e64572b2e21e43124857c4979",
+    "url": "/static/media/services-bg.97891b5e.jpg"
+  }
+]);
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './styles.css'
-import Scene from './Scene'
-
-window.scene = new Scene()
